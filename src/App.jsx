4f7da4ea27857707194f5dd570df56407b24afe2
itanaysharma <thisistanaysharma@gmail.com>
2024-02-24 18:35:40 +0530

    added all the Atoms

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { RecoilRoot, useRecoilValue } from "recoil";
-import { networkAtom } from "./atoms";
+import {
+  jobsAtom,
+  messagingAtom,
+  networkAtom,
+  notificationAtom,
+} from "./atoms";
 
 function App() {
   return (
@@ -14,6 +19,9 @@ function App() {
 }
 function MainApp() {
   const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationAtomCount = useRecoilValue(notificationAtom);
+  const messagingAtomCount = useRecoilValue(messagingAtom);
   return (
     <div>
       <button>Home</button>
@@ -21,9 +29,9 @@ function MainApp() {
         My Network (
         {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
       </button>
-      <button>Jobs</button>
-      <button>Messages</button>
-      <button>Notifications</button>
+      <button>Jobs ({jobsAtomCount})</button>
+      <button>Messages ({messagingAtomCount})</button>
+      <button>Notifications ({notificationAtomCount})</button>
       <button>Me</button>
     </div>
   );
